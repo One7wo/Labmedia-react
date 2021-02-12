12b5a2c52a6be5abc6d127b31b85c286149ba559
OneTwo <gonchar.dev@yandex.ru>
2021-02-12 21:06:36 +0300

    start dev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import './App.css'
+import './App.scss'
+import Filter from './assets/svg/filter (1).svg'
+
+export default function App() {
+    return (
+        <div>
+            <div className="header__title inner">Список пользователей</div>
+            <div className="search-form inner">
+                <div className="search-form__field">
+                    <input type="text" placeholder="Поиск по имени или e-mail"></input>
+                </div>
+                <div className="search-form__clear-btn">
+                    <img src={Filter} alt="img" />
+                    <span className="text">Очистить фильтр</span>
+                </div>
+            </div>
+            <div className="sort-buttons inner">
+                <div className="sort-buttons__text text">Сортировка:</div>
+                <div className="sort-buttons__date text active">Дата регистрации</div>
+                <div className="sort-buttons__rating text">Рейтинг</div>
+            </div>
+            <div className="users-table inner">
+                <div className="users-table__header">
+                    <div className="name text">Имя пользователя</div>
+                    <div className="email text">Email</div>
+                    <div className="date text">Дата регистрации</div>
+                    <div className="rating text">Рейтинг</div>
+                    <div className="remove">&#10006;</div>
+                </div>
+                <div className="users-table__user-info">
+                    <div className="name">Username</div>
+                    <div className="email text">mail@mail.ru</div>
+                    <div className="date text">11.11.1111</div>
+                    <div className="rating text">228</div>
+                    <div className="delete-btn">&#10006;</div>
+                </div>
+                <div className="users-table__user-info">
+                    <div className="name">Username</div>
+                    <div className="email text">mail@mail.ru</div>
+                    <div className="date text">11.11.1111</div>
+                    <div className="rating text">228</div>
+                    <div className="delete-btn">&#10006;</div>
+                </div>
+                <div className="users-table__user-info">
+                    <div className="name">Username</div>
+                    <div className="email text">mail@mail.ru</div>
+                    <div className="date text">11.11.1111</div>
+                    <div className="rating text">228</div>
+                    <div className="delete-btn">&#10006;</div>
+                </div>
+                <div className="users-table__user-info">
+                    <div className="name">Username</div>
+                    <div className="email text">mail@mail.ru</div>
+                    <div className="date text">11.11.1111</div>
+                    <div className="rating text">228</div>
+                    <div className="delete-btn">&#10006;</div>
+                </div>
+                <div className="users-table__user-info">
+                    <div className="name">Username</div>
+                    <div className="email text">mail@mail.ru</div>
+                    <div className="date text">11.11.1111</div>
+                    <div className="rating text">228</div>
+                    <div className="delete-btn">&#10006;</div>
+                </div>
+            </div>
+        </div >
+    )
 }
-
-export default App;
